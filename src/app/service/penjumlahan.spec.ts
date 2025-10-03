8a78e85bf1e051c9a2247884d1ad2bb7243ce579
infovegetarian <infovegetarian.id@gmail.com>
2025-10-03 20:27:58 +0700

    Update layout: two cards side-by-side for calculations, full-width card for data

--- src/app/service/penjumlahan.spec.ts
+++ src/app/service/penjumlahan.spec.ts
@@ -1,13 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
-import { Penjumlahan } from './penjumlahan';
+import { Penjumlahan1 } from './penjumlahan';
 
 describe('Penjumlahan', () => {
-  let service: Penjumlahan;
+  let service: Penjumlahan1;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(Penjumlahan);
+    service = TestBed.inject(Penjumlahan1);
   });
 
   it('should be created', () => {
