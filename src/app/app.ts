fa210c39a794b68df160bb6b8b164b37da6787bd
infovegetarian <infovegetarian.id@gmail.com>
2025-10-03 20:33:29 +0700

    Remove Material cards, use simple sections with vertical layout

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,14 +2,13 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
-import { MatCardModule } from '@angular/material/card';
 import { Segitigaluas } from "./components/segitigaluas/segitigaluas";
 import { Penjumlahan } from "./components/penjumlahan/penjumlahan";
 import { TampildataComponent } from "./components/tampildata/tampildata";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatToolbarModule, MatIconModule, MatCardModule, Segitigaluas, Penjumlahan, TampildataComponent],
+  imports: [RouterOutlet, MatToolbarModule, MatIconModule, Segitigaluas, Penjumlahan, TampildataComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
